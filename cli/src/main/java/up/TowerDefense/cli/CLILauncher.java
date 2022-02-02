fb04d8cffb2a7cb30d70e0e804d2a8ecf02dc5c3
lucasi <ivan.lucasreiner@gmail.com>
2022-02-02 16:09:23 +0100

    Implementation GameWindow, HomePanel + creation thread (timeloop)

--- cli/src/main/java/up/TowerDefense/cli/CLILauncher.java
+++ cli/src/main/java/up/TowerDefense/cli/CLILauncher.java
@@ -7,14 +7,7 @@ public class CLILauncher {
 
     public static void main(String[] args) {
 
-        JFrame gameWindow = new JFrame();
-        gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        gameWindow.setLocationRelativeTo(null);
-
-        GamePanel gamePanel = new GamePanel();
-        gameWindow.add(gamePanel);
-
-        gameWindow.setVisible(true);
+        GameWindow gameWindow = new GameWindow();
     }
 
 }
