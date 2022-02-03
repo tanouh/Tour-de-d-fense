8765132c3e6c338754557d1b34dfc1b4248da40a
MAHANDRY tania razanakoto <taniamahandry24@gmail.com>
2022-02-03 17:23:33 +0100

    Suite MapPanel

--- view/src/main/java/up/TowerDefense/view/MapPanel.java
+++ view/src/main/java/up/TowerDefense/view/MapPanel.java
@@ -4,14 +4,33 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class MapPanel extends JPanel {
     BufferedImage base ;
     public MapPanel(String image){
-        try{
-            base  = ImageIO.read()
-        }
+        loadImage(image);
+
         this.setLayout(new GridBagLayout());
+    }
+
+    private void loadImage(String img){
+        try{
+            base  = ImageIO.read(getClass().getResourceAsStream(img));
+
+            int imgWidth = base.getWidth();
+            int imgHeight = base.getHeight();
+
+            Color color ;
+             for (int col = 0 ; col < imgWidth ;col++ ){
+                 for (int row = 0 ; row < imgHeight; row++)
+                     color = new Color(base.getRGB(col, row), false);
+
+
+             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
