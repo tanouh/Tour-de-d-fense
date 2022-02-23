6048993344f5f24d35381cfd8f7a30b215a93a1f
lucasi <ivan.lucasreiner@gmail.com>
2022-02-23 20:25:02 +0100

    Ajout sideMenu dans GamePanel

--- view/src/main/java/up/TowerDefense/view/GamePanel.java
+++ view/src/main/java/up/TowerDefense/view/GamePanel.java
@@ -2,14 +2,17 @@ package up.TowerDefense.view;
 
 import up.TowerDefense.model.game.Player;
 
+import javax.sound.sampled.Line;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
 
 public class GamePanel extends JPanel {
+    private GameWindow gameWindow;
 
     //Mettre les contenus de label dans une classe "partie"
     private JPanel header = new JPanel(new GridLayout(1,4));
+    private JPanel sideMenu = new JPanel();
     private JPanel body = new JPanel();
     private Color background = new Color(173,175,192);
     private Color foreground = new Color(30,35,71);
@@ -17,7 +20,18 @@ public class GamePanel extends JPanel {
 
 
     public GamePanel(GameWindow gameWindow){
+        this.gameWindow = gameWindow;
         this.setLayout(new BorderLayout());
+
+        this.setHeader();
+        this.setSideMenu();
+        this.add(header, BorderLayout.NORTH);
+        this.add(body, BorderLayout.CENTER);
+        this.add(sideMenu, BorderLayout.EAST);
+
+    }
+
+    public void setHeader(){
         header.setBackground(background);
         header.setBorder(new LineBorder(foreground, 5));
 
@@ -30,19 +44,23 @@ public class GamePanel extends JPanel {
         title.setBorder(new LineBorder(foreground, 2));
         header.add(title);
 
-
-//        Label numWave = new JLabel();
-//        Label ennemyLeft = new JLabel();
-//        Label money = new JLabel();
         Label.addHeaderLabel(header,"Vague numero 0");
         Label.addHeaderLabel(header, "Ennemis restant : ");
         Label.addHeaderLabel(header,"Argent : " + Player.getCredits());
         Label.addHeaderLabel(header, "Vies : " + Player.getLives());
         body.add(new ScreenPanel());
-
-        this.add(header, BorderLayout.NORTH);
-        this.add(body, BorderLayout.CENTER);
-
     }
 
+    public void setSideMenu(){
+        sideMenu.setBackground(background);
+        sideMenu.setBorder(new LineBorder(foreground, 5));
+        sideMenu.setPreferredSize(new Dimension(gameWindow.getWidth()/5, gameWindow.getHeight()/5));
+
+        JLabel title = new JLabel("Construction Tour");
+        title.setFont(new Font("Bernard MT Condensed",Font.BOLD, 20));
+        title.setForeground(foreground);
+        title.setHorizontalAlignment(JLabel.CENTER);
+        title.setVerticalAlignment(JLabel.CENTER);
+        sideMenu.add(title);
+    }
 }
--- view/src/main/java/up/TowerDefense/view/ScreenPanel.java
+++ view/src/main/java/up/TowerDefense/view/ScreenPanel.java
@@ -1,7 +1,5 @@
 package up.TowerDefense.view;
 
-
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
