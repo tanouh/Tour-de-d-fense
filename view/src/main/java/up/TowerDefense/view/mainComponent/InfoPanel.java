416d94924f8c2c32252cef2637c0cb411c007b1a
lucasi <ivan.lucasreiner@gmail.com>
2022-03-21 20:52:37 +0100

    Ajout fichier infos.txt (impossible à lire pour l'instant)

--- view/src/main/java/up/TowerDefense/view/mainComponent/InfoPanel.java
+++ view/src/main/java/up/TowerDefense/view/mainComponent/InfoPanel.java
@@ -2,6 +2,10 @@ package up.TowerDefense.view.mainComponent;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class InfoPanel extends JPanel {
     private Color background = new Color(173,175,192);
@@ -11,25 +15,39 @@ public class InfoPanel extends JPanel {
         this.setBackground(background);
         this.setForeground(foreground);
         JLabel infoDisplay;
-        switch (currentInfo){
-            case 1 :
-                infoDisplay = new JLabel("Information sur les TowerTest");
-                break;
-            case 2 :
-                infoDisplay = new JLabel("Information sur les Tours anti-champis");
-                break;
-            case 3 :
-                infoDisplay = new JLabel("Information sur les Leucocyte T");
-                break;
-            case 4 :
-                infoDisplay = new JLabel("Information sur les Anticorps");
-                break;
-            case 5 :
-                infoDisplay = new JLabel("Information sur les Murs");
-                break;
-            default :
-                infoDisplay = new JLabel("erreur");
-                break;
+//        switch (currentInfo){
+//            case 1 :
+//                infoDisplay = new JLabel("Information sur les TowerTest");
+//                break;
+//            case 2 :
+//                infoDisplay = new JLabel("Information sur les Tours anti-champis");
+//                break;
+//            case 3 :
+//                infoDisplay = new JLabel("Information sur les Leucocyte T");
+//                break;
+//            case 4 :
+//                infoDisplay = new JLabel("Information sur les Anticorps");
+//                break;
+//            case 5 :
+//                infoDisplay = new JLabel("Information sur les Murs");
+//                break;
+//            default :
+//                infoDisplay = new JLabel("erreur");
+//                break;
+//        }
+        try(BufferedReader br = new BufferedReader(new FileReader("infos.txt")))
+        {
+            String line = br.readLine();
+            for (int i = 0; i<currentInfo; i++){
+                while (line != "*"){
+                    br.readLine();
+                }
+            }
+            infoDisplay = new JLabel(br.readLine());
+        }
+        catch (IOException e) {
+            infoDisplay = new JLabel("An error occurred.");
+            e.printStackTrace();
         }
         infoDisplay.setForeground(foreground);
         infoDisplay.setFont(new Font("Bernard MT Condensed",Font.PLAIN, 20));
