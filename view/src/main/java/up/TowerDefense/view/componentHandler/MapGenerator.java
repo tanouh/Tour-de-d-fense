89261a2a05d584283d919450b613bed61d3920f1
MAHANDRY tania razanakoto <taniamahandry24@gmail.com>
2022-03-31 18:36:03 +0200

    Fonctions d'attaques des ennemis (début)

--- view/src/main/java/up/TowerDefense/view/componentHandler/MapGenerator.java
+++ view/src/main/java/up/TowerDefense/view/componentHandler/MapGenerator.java
@@ -261,7 +261,7 @@ public class MapGenerator {
      * Mets à jour le chemin suivi par l'enemi, cette fonction est appelée à chaque fois qu'une tour a été placée ou supprimée de la carte
      */
     private void updateCharactersPaths() {
-        for (Personnage c : charactersList){
+        for (Personnage c : new CopyOnWriteArrayList<>(charactersList)){
             if(c instanceof Enemy){
                 ((Enemy)c).update_paths();
             }
@@ -271,8 +271,8 @@ public class MapGenerator {
     /**
      * Actualise la position de l'enemi suivant le chemin qu'il est entrain de suivre
      */
-    public void updateCharactersPositions() {
-        for (Personnage c : charactersList){
+    public synchronized void updateCharactersPositions() {
+        for (Personnage c : new CopyOnWriteArrayList<>(charactersList)){
             if(c instanceof Enemy){
                 ((Enemy)c).update_position();
             }
